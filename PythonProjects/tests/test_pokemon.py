c4e0426d4550fc7b3892af9f011df82b99272a8e
Zharlgasov Akezhan <127430547+akebro@users.noreply.github.com>
2024-04-11 22:31:08 +0400

    Add files via upload

--- PythonProjects/tests/test_pokemon.py
+++ PythonProjects/tests/test_pokemon.py
@@ -1,1 +1,20 @@
+import requests
+import pytest
 
+URL = 'https://api.pokemonbattle.me/v2'
+
+HEADERS = {
+    'Content-Type' : 'application/json',
+    'trainer_token' : '3ddcca0fff3d7abaa1f2870cd1b4cb3b'
+    }
+
+TOKEN = '3ddcca0fff3d7abaa1f2870cd1b4cb3b'
+
+def test_status_code():
+    response = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : 2313})
+    assert response.status_code == 200
+    
+
+def test_part_of_response():
+        response = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : 2313})
+        assert response.json()['data'][0]['trainer_name'] == 'Рак'
